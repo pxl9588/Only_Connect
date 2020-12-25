6aa16aa90b38271c96c93de7339bf9fda34ed4ff
Patrick Landers <pxl9588@g.rit.edu>
2020-12-25 15:15:59 -0500

    Cleaner game state

--- only_connect/src/Components/Game.js
+++ only_connect/src/Components/Game.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import PSWall from './PSWall';
 import WordWall from './WordWall';
-import PSRow from './PSRow';
+import ConnectionRow from './ConnectionRow';
+import SequenceRow from './SequenceRow';
 import MissingVowels from './MissingVowels';
 import WordWallIcons from './WorldWallIcons';
 
@@ -20,9 +21,9 @@ function Game({...props})
     const connectionWall = [["A", "B", "C", "D","Letter"], ["E", "F", "G", "H","Letter"], ["I", "J", "K", "L","Letter"], ["M", "N", "O", "P","Letter"], ["Q", "R", "S", "T","Letter"], ["U", "V", "W", "X","Letter"]];
     const sequenceWall = [["1", "2", "3", "4","Numbers"], ["5", "6", "7", "8","Numbers"], ["9", "10", "11", "12","Numbers"], ["13", "14", "15", "16","Numbers"], ["17", "18", "19", "20","Numbers"], ["21", "22", "23", "24","Numbers"]];
 
+    // Click handles
     const psWallHandle = (i) =>
     {
-        console.log(`Coming back from ${i}`);
         var temp = {...gameState.hidden};
         temp[i] = true;
 
@@ -34,7 +35,6 @@ function Game({...props})
         if(gameState.wallIndex === 5)
         {
             setGameState({...gameState, wallIndex: 0, clickedRow: false, round: gameState.round + 1, hidden:{1: false, 2:false, 3:false, 4:false, 5:false, 6:false}});
-            console.log("End of Round 1, going to Round 2");
         }
         else
         {
@@ -43,19 +43,27 @@ function Game({...props})
         
     }
 
-    var div = null;
+    const renderSwitch = (round) =>
+    {
+        switch (gameState.round)
+        {
+            case 0:
+                return <div>{gameState.clickedRow === false ? <PSWall onClick={psWallHandle} hidden={gameState.hidden}></PSWall> : <ConnectionRow exitClick={psRowExit} row={connectionWall[gameState.wallIndex]}></ConnectionRow>}</div>
+            case 1:
+                return <div>{gameState.clickedRow === false ? <PSWall onClick={psWallHandle} hidden={gameState.hidden}></PSWall> : <SequenceRow exitClick={psRowExit} row={sequenceWall[gameState.wallIndex]}></SequenceRow>}</div>
+            case 2:
+                return <div>{gameState.clickedRow === false ? <WordWallIcons onClick={psWallHandle} hidden={gameState.hidden}></WordWallIcons> : <WordWall> </WordWall>}</div>
+            default:
+                return <div>round {round} not implemented</div>;
+        }
+    }
 
-    if(gameState.round < 2)
-    {
-        return(
-            <div>{gameState.clickedRow === false ? <PSWall onClick={psWallHandle} hidden={gameState.hidden}></PSWall> : <PSRow exitClick={psRowExit} type={gameState.round === 0 ? "" : "sequence"} row={gameState.round === 0 ? connectionWall[gameState.wallIndex] : sequenceWall[gameState.wallIndex]}></PSRow>}</div>
+    
+
+    return (
+        <div>
+        {renderSwitch(gameState.round)}
+        </div>
         )
-    }
-    else
-    {
-        return(
-            <div>{gameState.clickedRow === false ? <WordWallIcons onClick={psWallHandle} hidden={gameState.hidden}></WordWallIcons> : <WordWall> </WordWall>}</div>
-        )
-    }
 }
 export default Game;
