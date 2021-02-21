dd7bac6d9985a52da81fd9e3aa27b837b8c5321c
akats7 <56042997+akats7@users.noreply.github.com>
2021-02-20 23:31:19 -0500

    Refactored lives code

--- only_connect/src/Components/Lives.js
+++ only_connect/src/Components/Lives.js
@@ -2,38 +2,17 @@ import React from "react";
 import dot from "../images/orange-dot.png";
 
 export default function Lives(props) {
-    return (
-        <div style={{ display: "flex" }}>
-            {props.lives > 0 ? (
-                <img
-                    style={{ left: "15%", position: "relative" }}
-                    className={`h-12 w-12 pointer-events-none`}
-                    src={dot}
-                    alt={dot}
-                ></img>
-            ) : (
-                ""
-            )}
-            {props.lives > 1 ? (
-                <img
-                    style={{ left: "25%", position: "relative" }}
-                    className={`h-12 w-12 pointer-events-none`}
-                    src={dot}
-                    alt={dot}
-                ></img>
-            ) : (
-                ""
-            )}
-            {props.lives > 2 ? (
-                <img
-                    style={{ left: "35%", position: "relative" }}
-                    className={`h-12 w-12 pointer-events-none`}
-                    src={dot}
-                    alt={dot}
-                ></img>
-            ) : (
-                ""
-            )}
-        </div>
-    );
+    const lives = [];
+    for (let i = 0; i < props.lives; i++) {
+        lives.push(
+            <img
+                style={{ left: `${15 + 10 * i}%`, position: "relative" }}
+                className={`h-12 w-12 pointer-events-none`}
+                src={dot}
+                alt={dot}
+            ></img>,
+        );
+    }
+
+    return <div className="flex">{lives}</div>;
 }
