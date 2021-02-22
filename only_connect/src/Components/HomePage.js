33cf9fa7e7e43dfe9c8b3b54c867fb4a09713a44
akats7 <56042997+akats7@users.noreply.github.com>
2021-02-21 21:02:30 -0500

    Improved Home Page

--- only_connect/src/Components/HomePage.js
+++ only_connect/src/Components/HomePage.js
@@ -2,6 +2,9 @@ import React from "react";
 import OverrideButton from "./OverrideButton";
 import TextField from "@material-ui/core/TextField";
 import ButtonCorrect from "./ButtonCorrect";
+import Whale from '../images/whale.png'
+import Seal from '../images/seal.png'
+
 
 export default function HomePage(props) {
     return (
@@ -10,17 +13,22 @@ export default function HomePage(props) {
             <div className="mt-20 flex justify-center">
                 <div style={{ left: `-5%`, position: "relative" }}>
                     <h2 className="text-3xl text-center">Team 1:</h2>
+                    <img src={Whale}></img>
+                    <div className='justify-center flex'>
                     <TextField
-                        className="text-9xl text-center"
                         size={"medium"}
                         value={"Hello"}
                         margin="normal"
                         autoFocus
                     ></TextField>
+                    </div>
                 </div>
                 <div style={{ left: `$5%`, position: "relative" }}>
                     <h2 className="text-3xl text-center">Team2:</h2>
+                    <img src={Seal}></img>
+                    <div className='flex justify-center '>
                     <TextField value={"Hello"} margin="normal" autoFocus></TextField>
+                    </div>
                 </div>
             </div>
             <div
