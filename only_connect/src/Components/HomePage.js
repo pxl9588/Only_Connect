ec54f6580b70f0e37cc1d300d0b74c5fa8cffe56
akats7 <56042997+akats7@users.noreply.github.com>
2021-02-23 21:26:12 -0500

    firebase

--- only_connect/src/Components/HomePage.js
+++ only_connect/src/Components/HomePage.js
@@ -4,6 +4,28 @@ import TextField from "@material-ui/core/TextField";
 import ButtonCorrect from "./ButtonCorrect";
 import Whale from '../images/whale.png'
 import Seal from '../images/seal.png'
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBVW4nxJbj2K0sLhzMM8yLUardBYR6hIrs",
+    authDomain: "ocbackend.firebaseapp.com",
+    databaseURL: "https://ocbackend-default-rtdb.firebaseio.com",
+    projectId: "ocbackend",
+    storageBucket: "ocbackend.appspot.com",
+    messagingSenderId: "614462186004",
+    appId: "1:614462186004:web:e2a77517a15b91006a3d01",
+    measurementId: "G-2MB7M8V461"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  function writeUserData(name, email) {
+    database.ref('user2/').set({
+      username: name,
+      email: email
+    });
+  }
 
 
 export default function HomePage(props) {
@@ -37,7 +59,7 @@ export default function HomePage(props) {
                 style={{ left: `-2%`, position: "relative" }}
                 className="mt-20 flex justify-center"
             >
-                <OverrideButton onClick={props.startGame}>Start Game</OverrideButton>
+                <OverrideButton onClick={() => {writeUserData('Bloop','Blap')}}>Start Game</OverrideButton>
             </div>
         </div>
     );
