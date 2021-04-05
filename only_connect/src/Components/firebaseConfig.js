437bcff7eb4fd715b466e00b111fa37ae0615685
akats7 <56042997+akats7@users.noreply.github.com>
2021-04-05 18:11:11 -0400

    Firebase config update

--- only_connect/src/Components/firebaseConfig.js
+++ only_connect/src/Components/firebaseConfig.js
@@ -3,15 +3,15 @@ import database from "firebase/database";
 import "firebase/auth";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBVW4nxJbj2K0sLhzMM8yLUardBYR6hIrs",
-    authDomain: "ocbackend.firebaseapp.com",
-    databaseURL: "https://ocbackend-default-rtdb.firebaseio.com",
-    projectId: "ocbackend",
-    storageBucket: "ocbackend.appspot.com",
-    messagingSenderId: "614462186004",
-    appId: "1:614462186004:web:e2a77517a15b91006a3d01",
-    measurementId: "G-2MB7M8V461",
-};
+    apiKey: "AIzaSyD5sYUDmjtZQl11SMpVepn_CHLOSdkKoRQ",
+    authDomain: "sole-relation.firebaseapp.com",
+    databaseURL: "https://sole-relation-default-rtdb.firebaseio.com",
+    projectId: "sole-relation",
+    storageBucket: "sole-relation.appspot.com",
+    messagingSenderId: "1081140521353",
+    appId: "1:1081140521353:web:27b1c18cdafb0f41611958",
+    measurementId: "G-TX2X8JPJD3"
+  };
 // Initialize Firebase
 
 function initFireBase() {
