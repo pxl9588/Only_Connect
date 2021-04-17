f827ca0a0cfc5a10a3f7bee3bb8d041385179668
Patrick Landers <pxl9588@g.rit.edu>
2021-04-17 18:34:10 -0400

    Added missing questions to Connection Row and Sequence Rows

--- only_connect/src/Components/CreateGame.js
+++ only_connect/src/Components/CreateGame.js
@@ -31,12 +31,14 @@ function CreateGame(props)
         var val = e.target.value;
         var tempInputVal = [...inputVal];
         //Everything else
-        if(page < 16)
+        if(page < 20)
         {
+            // This records the answer
             if(i === 4)
             {
                 setData({...data, answer: val})
             }   
+            // This records the clues
             else
             {
                 var tempClues = data.clues;
@@ -80,17 +82,17 @@ function CreateGame(props)
         var inputs = []
         for(var i = 0; i < 5; i++)
         {
-            inputs.push(<div className={`${page > 15 ? "grid grid-cols-1 md:flex" : "my-5"}  md:m-5`} key={i}>
-                {i < 4 ? "Clue #"+(i+1) : page > 15 ? "Category" : "Answer"}
+            inputs.push(<div className={`${page >= 20 ? "grid grid-cols-1 md:flex" : "my-5"}  md:m-5`} key={i}>
+                {i < 4 ? "Clue #"+(i+1) : page >= 20 ? "Category" : "Answer"}
                 <input
                 type="text"
                 key={i}
                 value={inputVal[i]}
                 onInput={collectData.bind(this, i)}
-                placeholder={i < 4 ? "Clue #"+(i+1) : page > 15 ? "Category" : "Answer"}
+                placeholder={i < 4 ? "Clue #"+(i+1) : page >= 20 ? "Category" : "Answer"}
                 className="mx-4 px-2 h-10 placeholder-gray-400 text-gray-700 relative bg-white rounded text-md shadow outline-none focus:outline-none focus:shadow-outline"></input>
                 {
-                    (page > 15 && i !== 4) ? <input
+                    (page >= 20 && i !== 4) ? <input
                     type="text"
                     key={5+i}
                     value={inputVal[5+i]}
@@ -109,36 +111,36 @@ function CreateGame(props)
     {
         var tempGameData = {...gameData};
         // Connection Rows
-        if(page < 4)
+        if(page < 6)
         {
             tempGameData.connections.push(data);
             setGameData(tempGameData);
             setData(newData);
         }
-        else if(page < 8)
+        else if(page < 12)
         {
             tempGameData.sequences.push(data);
             setGameData(tempGameData);
             setData(newData);
         }
-        else if(page < 12)
+        else if(page < 16)
         {
             tempGameData.wall.wall1.push(data);
             setGameData(tempGameData);
             setData(newData);
         }
-        else if(page < 16)
+        else if(page < 20)
         {
             tempGameData.wall.wall2.push(data);
             setGameData(tempGameData);
             setData(newData);
         }
-        else if(page < 20)
+        else if(page < 24)
         {
             tempGameData.missingVowels.push(mvData);
             setGameData(tempGameData);
             setMVData(newMVData);
-            if(page === 19)
+            if(page === 23)
             {
                 downloadFile();
             }
@@ -168,23 +170,23 @@ function CreateGame(props)
     const generateHeader = (i) =>
     {
         var content = ""
-        if(i < 4)
+        if(i < 6)
         {
             content = `Connection Row #${i+1}`;
         }
-        else if(i < 8)
-        {
-            content = `Sequence Row #${(i%4)+1}`;
-        }
         else if(i < 12)
         {
-            content = `Word Wall 1 #${(i%4)+1}`;
+            content = `Sequence Row #${(i%6)+1}`;
         }
         else if(i < 16)
         {
-            content = `Word Wall 2 #${(i%4)+1}`;
+            content = `Word Wall 1 #${(i%6)+1}`;
         }
         else if(i < 20)
+        {
+            content = `Word Wall 2 #${(i%4)+1}`;
+        }
+        else if(i < 24)
         {
             content = `Missing Vowels #${(i%4)+1}`;
         }
@@ -203,11 +205,11 @@ function CreateGame(props)
                     generateHeader(page)
                 }
                 {
-                    page < 20 ?
+                    page < 24 ?
                     generateInputs() : ""
                 }
                 {
-                    page < 20 ?
+                    page < 24 ?
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold h-10 w-40 px-2 rounded m-4"
                     onClick={submit}>
                         Submit
